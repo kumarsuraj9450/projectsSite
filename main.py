06675f0310829fadd94f8b56fa73e50846db6bed
kumarsuraj9450 <kumarsuraj9450@gmail.com>
2020-11-04 22:21:10 +0530

    added url page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional, List
 from os import getcwd
+from os.path import basename
 # from fastapi import FastAPI
 
 # app = FastAPI()
@@ -21,6 +22,8 @@ from fastapi.templating import Jinja2Templates
 from shutil import copyfileobj
 from fastai.vision import load_learner, open_image
 from pickle import load
+import requests
+from urllib.parse import urlparse
 
 app = FastAPI()
 
@@ -89,9 +92,9 @@ def predict(name):
 def post_item(request: Request, files: UploadFile = File(...)):
 	try:
 		if not "image" in str(files.content_type):
-			raise HTTPException(status_code=404, detail="Something wrong with the file uploaded file")
+			raise HTTPException(status_code=404, detail="Not an image file")
 	except:
-		raise HTTPException(status_code=404, detail="Something wrong with the file uploaded file")
+		raise HTTPException(status_code=404, detail="Not an image file")
 
 	with open(f"static/userFiles/{files.filename}", "wb") as buffer:
 		copyfileobj(files.file, buffer)
@@ -106,3 +109,40 @@ def post_item(request: Request, files: UploadFile = File(...)):
 		)
 			# return {"filename": image.filename}
 	# return {"file":files.filename, "type":files.content_type}
+
+@app.get("/classify_url", response_class=HTMLResponse)
+def read_url(request: Request):
+	return templates.TemplateResponse("classify_url.html", {"request": request})
+
+@app.post("/classify_url")
+def post_url(request: Request, files: str = Form(...)):
+	print(type(files))
+	try:
+		resp = requests.get(files, stream=True)
+
+		if not "image" in resp.headers['content-type'] : 
+			del resp
+			raise HTTPException(status_code=404, detail="Not an image file")
+		
+		a = urlparse(files)
+		name = basename(a.path)
+		with open(f"static/userFiles/{name}", 'wb') as local_file:
+			resp.raw.decode_content = True
+			copyfileobj(resp.raw, local_file)
+
+		pred = predict(f"\\static\\userFiles\\{name}")
+		# local_file = open(name, 'wb')
+		# resp.raw.decode_content = True
+		# copyfileobj(resp.raw, local_file)
+		# local_file.close()
+	except:
+		url = "Nothing happend"
+		return {'res':"OMG"}
+	# return files
+
+	return templates.TemplateResponse("vision.html", 
+		{"request": request, 
+		"image":f"static/userFiles/{name}", 
+		"file":name , 
+		"prediction":pred}
+		)
--- test.py
+++ test.py
@@ -1,29 +1,71 @@
-from fastai.vision import load_learner, open_image
+# from fastai.vision import load_learner, open_image
 
-from os import getcwd
-from pickle load
+# from os import getcwd
+# from pickle load
 
-cwd = os.getcwd()
+# cwd = os.getcwd()
 
-name = r"\static\c06bn5a1ziw51.jpg"
+# name = r"\static\c06bn5a1ziw51.jpg"
 
-url = cwd + name
+# url = cwd + name
 
-learner = load_learner('.',r'stage-1-export-file.pkl')
+# learner = load_learner('.',r'stage-1-export-file.pkl')
 
-# print(learner.data.classes)
-
-
-im = open_image(url)
-
-with open('classes.pkl', 'rb') as f:
-    data = load(f)
-
-res = learner.predict(im)
-predictedClass = data[res[1]]
-print(predictedClass)
+# # print(learner.data.classes)
 
 
+# im = open_image(url)
+
+# with open('classes.pkl', 'rb') as f:
+#     data = load(f)
+
+# res = learner.predict(im)
+# predictedClass = data[res[1]]
+# print(predictedClass)
+
+# Import requests, shutil python module.
+import requests
+
+import shutil
+
+# This is the image url.
+# image_url = "https://en.wikipedia.org/wiki/Cat_intelligence#/media/File:An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg"
+
+image_url = "https://www.dev2qa.com/demo/images/green_button.jpg"
+image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg/1024px-An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg"
+# Open the url image, set stream to True, this will return the stream content.
+# resp = requests.get(image_url, stream=True)
+
+# print(resp.image_url)
+
+# Open a local file with wb ( write binary ) permission.
+# local_file = open('local_image.jpg', 'wb')
+
+# # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+# resp.raw.decode_content = True
+
+# # Copy the response stream raw data to local image file.
+# shutil.copyfileobj(resp.raw, local_file)
+
+# # Remove the image url response object.
+# del resp
 
 
 
+import os
+from urllib.parse import urlparse
+
+url = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg/1024px-An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg"
+
+a = urlparse(url)
+
+print(a.path)
+print(os.path.basename(a.path))
+name = os.path.basename(a.path)
+resp = requests.get(url, stream=True)
+local_file = open(name, 'wb')
+resp.raw.decode_content = True
+shutil.copyfileobj(resp.raw, local_file)
+local_file.close()
+print(hash(name))
+# local_file = open('local_image.jpg', 'wb')
